7389b3b4ee4407bafbff15db0af0569553812024
rustamlilala <rustamlilala@gmail.com>
2017-09-14 12:11:01 +0400

    wishlist added compeletely

--- Project/shop/IdmanistFinal/Idmanist.Web/Controllers/HomeController.cs
+++ Project/shop/IdmanistFinal/Idmanist.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using IdmanistData.DataContext;
 using IdmanistData.Model;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,7 +18,7 @@ namespace Idmanist.Web.Controllers
         private readonly ISliderRepository _sliderRepository;
         private readonly IWishRepository _wishRepository;
 
-        public HomeController(ICategoryRepository categoryRepository,IProductRepository productRepository, ISliderRepository sliderRepository,IWishRepository wishRepository)
+        public HomeController(ICategoryRepository categoryRepository, IProductRepository productRepository, ISliderRepository sliderRepository, IWishRepository wishRepository)
         {
             _categoryRepository = categoryRepository;
             _productRepository = productRepository;
@@ -35,6 +36,8 @@ namespace Idmanist.Web.Controllers
 
             return View(model);
         }
+
+
         public string UnlikeThis(int id)
         {
             using (IdmanistDataContext db = new IdmanistDataContext())
@@ -56,7 +59,37 @@ namespace Idmanist.Web.Controllers
         }
 
 
+        public string LikeThis(int id)
+        {
+             
+ 
+             using (IdmanistDataContext db = new IdmanistDataContext())
+             {
+                 dynamic mymodel = new ExpandoObject();
+                 Product prod = db.Product.FirstOrDefault(x => x.ProductId == id);
+                
+                 if (User.Identity.IsAuthenticated || Session["user"] != null)
+                 {
+                     Customer thisUser = (Customer)(Session["user"]);
+                     Customer e = db.Customer.FirstOrDefault(x => x.Email == thisUser.Email);
+                     prod.mywishes++;
+                     
+                     Wishes like = new Wishes();
+                     like.product_id = id;
+                     like.customer_id = e.UserID;
+                     like.wished = true;
+                     db.Wishes.Add(like);
+                     db.SaveChanges();
+ 
+                 }
+ 
+                 return prod.mywishes.ToString();
+             }
+
 
 
     }
+
+
+    }      
 }
---
+++ Project/shop/IdmanistFinal/Idmanist.Web/Scripts/like.js
@@ -0,0 +1,29 @@
+﻿jQuery(document).ready(function () {
+//do the ajax call
+    jQuery(document).on("click", "a.like", function () {
+
+    var id = jQuery(this).data("id");
+    var link = "/Home/LikeThis/" + id;
+    var a = jQuery(this);
+    jQuery.ajax({
+        type: "GET",
+        url: link,
+        success: function (result) {
+            a.html('<i class="fa fa-heart fa-lg text-danger"></i> (' + result + ')').removeClass("like").addClass("unlike");
+        }
+    });
+})
+    jQuery(document).on("click", "a.unlike", function () {
+    
+    var id = jQuery(this).data("id");
+    var link = "/Home/UnlikeThis/" + id;
+    var a = jQuery(this);
+    jQuery.ajax({
+        type: "GET",
+        url: link,
+        success: function (result) {
+            a.html('<i class="fa fa-heart-o fa-lg"></i> (' + result + ')').removeClass("unlike").addClass("like");;
+        }
+    });
+})
+    })
